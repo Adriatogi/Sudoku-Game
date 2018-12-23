2bd23345d8391461f03aca93c60c3c0fc5992497
Adrian Gamarra <Adriatogi@gmail.com>
2018-12-22 22:14:19 -0800

    Cleanned up and Commented New Javascript

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ var a = [$(".a1"), $(".a2"), $(".a3"),$(".a4"),$(".a5"),$(".a6"),$(".a7"),$(".a8
 var b = [$(".b1"), $(".b2"), $(".b3"), $(".b4"), $(".b5"), $(".b6"), $(".b7"), $(".b8"), $(".b9")];
 var i = 0;
 
-while(i < 35){
-  rowA = a[Math.floor(Math.random() * a.length)];
-  rowB = Math.floor(Math.random() * b.length);
-  var aChild = rowA[0].children[rowB].children[0];
-  randNumber = (Math.floor(Math.random() * 9) + 1);
-  if(aChild.value == ""){
-    aChild.value = randNumber;
+while(i < 35){ // Make sure at least 35 squares are filled
+  var rowA = a[Math.floor(Math.random() * a.length)]; // pick a random row
+  var rowB = Math.floor(Math.random() * b.length);
+  var randCell = rowA[0].children[rowB].children[0]; // pick random cell
+  randNumber = (Math.floor(Math.random() * 9) + 1); 
+  if(randCell.value == ""){ // if empty
+    randCell.value = randNumber; // Fill with random value
     i++;
   }
 }
