b539e69f40199a7eef5bc7988dc45464f2fb31b6
Adrian Gamarra <Adriatogi@gmail.com>
2018-12-23 16:01:44 -0800

    Numbers in a Column Can't Equal in Setup

--- js/main.js
+++ js/main.js
@@ -3,22 +3,32 @@ var b = [$(".b1"), $(".b2"), $(".b3"), $(".b4"), $(".b5"), $(".b6"), $(".b7"), $
 var i = 0;
 
 while(i < 35){ // Make sure at least 35 squares are filled
+  console.log("Start Over")
   var works = 1;
   var rowA = a[Math.floor(Math.random() * a.length)]; // pick a random row
   var rowB = Math.floor(Math.random() * b.length);
   var randCell = rowA[0].children[rowB].children[0]; // pick random cell
-  var cells = [].slice.call(rowA[0].children) // Create an array of every cel
+  var bCells = [].slice.call(rowA[0].children[rowB].children)
+  var aCells = [].slice.call(rowA[0].children) // Create an array of every cell
   randNumber = (Math.floor(Math.random() * 9) + 1); 
+
+  a.forEach(function(row){
+    if(randNumber == row[0].children[rowB].children[0].value){
+      works = 0;
+    }
+  });
   
-  cells.forEach(function(cell){
+  aCells.forEach(function(cell){
    if(randNumber == cell.children[0].value){ // if new value already exists in row, do not input
     works = 0;
    };
   });
 
-  if(randCell.value == "" && works ==1){ // if empty and not existing in the row
+  if((randCell.value == "") &&(works ==1)){ // if empty and not existing in the row
     randCell.value = randNumber; // Fill with random value
     i++;
+    console.log(randNumber)
+    console.log("Filled")
   }
 }
 
