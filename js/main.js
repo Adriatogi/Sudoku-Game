628096c37af917299a27a1409e39a0b69cf4b1b1
Adrian Gamarra <Adriatogi@gmail.com>
2018-12-23 17:13:24 -0800

    Numbers in a 3x3 Tile Cant Equal

--- js/main.js
+++ js/main.js
@@ -3,13 +3,12 @@ var b = [$(".b1"), $(".b2"), $(".b3"), $(".b4"), $(".b5"), $(".b6"), $(".b7"), $
 var i = 0;
 
 while(i < 35){ // Make sure at least 35 squares are filled
-  console.log("Start Over")
+  console.log("start over")
   var works = 1;
-  var rowA = a[Math.floor(Math.random() * a.length)]; // pick a random row
+  var rowA = Math.floor(Math.random() * a.length); // pick a random row
   var rowB = Math.floor(Math.random() * b.length);
-  var randCell = rowA[0].children[rowB].children[0]; // pick random cell
-  var bCells = [].slice.call(rowA[0].children[rowB].children)
-  var aCells = [].slice.call(rowA[0].children) // Create an array of every cell
+  var randCell = a[rowA][0].children[rowB].children[0]; // pick random cell
+  var aCells = [].slice.call(a[rowA][0].children) // Create an array of every cell
   randNumber = (Math.floor(Math.random() * 9) + 1); 
 
   a.forEach(function(row){
@@ -19,16 +18,141 @@ while(i < 35){ // Make sure at least 35 squares are filled
   });
   
   aCells.forEach(function(cell){
-   if(randNumber == cell.children[0].value){ // if new value already exists in row, do not input
-    works = 0;
-   };
+    if(randNumber == cell.children[0].value){ // if new value already exists in row, do not input
+     works = 0;
+    };
   });
 
-  if((randCell.value == "") &&(works ==1)){ // if empty and not existing in the row
-    randCell.value = randNumber; // Fill with random value
-    i++;
-    console.log(randNumber)
-    console.log("Filled")
+if(((rowA == 0) || (rowA == 3) || (rowA == 6)) && ((rowB == 0) || (rowB == 3) || (rowB == 6))){
+  if(randNumber == (a[rowA+1][0].children[rowB+1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA+1][0].children[rowB+2].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA+2][0].children[rowB+1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA+2][0].children[rowB+2].children[0].value)){
+    works = 0;
+  }
+}
+else if(((rowA == 1) || (rowA == 4) || (rowA == 7)) && ((rowB == 0) || (rowB == 3) || (rowB == 6))){
+  if(randNumber == (a[rowA+1][0].children[rowB+1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA+1][0].children[rowB+2].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA-1][0].children[rowB+1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA-1][0].children[rowB+2].children[0].value)){
+    works = 0;
+  }
+}
+else if(((rowA == 2) || (rowA == 5) || (rowA == 8)) && ((rowB == 0) || (rowB == 3) || (rowB == 6))){
+  if(randNumber == (a[rowA-2][0].children[rowB+1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA-2][0].children[rowB+2].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA-1][0].children[rowB+1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA-1][0].children[rowB+2].children[0].value)){
+    works = 0;
+  }
+}
+else if(((rowA == 0) || (rowA == 3) || (rowA == 6)) && ((rowB == 1) || (rowB == 4) || (rowB == 7))){
+  if(randNumber == (a[rowA+2][0].children[rowB+1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA+2][0].children[rowB-1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA+1][0].children[rowB+1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA+1][0].children[rowB-1].children[0].value)){
+    works = 0;
+  }
+}
+else if(((rowA == 0) || (rowA == 3) || (rowA == 6)) && ((rowB == 2) || (rowB == 5) || (rowB == 8))){
+  if(randNumber == (a[rowA+2][0].children[rowB-2].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA+2][0].children[rowB-1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA+1][0].children[rowB-2].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA+1][0].children[rowB-1].children[0].value)){
+    works = 0;
+  }
+}
+else if(((rowA == 1) || (rowA == 4) || (rowA == 7)) && ((rowB == 1) || (rowB == 4) || (rowB == 7))){
+  if(randNumber == (a[rowA-1][0].children[rowB+1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA-1][0].children[rowB-1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA+1][0].children[rowB+1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA+1][0].children[rowB-1].children[0].value)){
+    works = 0;
+  }
+}
+else if(((rowA == 1) || (rowA == 4) || (rowA == 7)) && ((rowB == 2) || (rowB == 5) || (rowB == 8))){
+  if(randNumber == (a[rowA-1][0].children[rowB-2].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA-1][0].children[rowB-1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA+1][0].children[rowB-2].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA+1][0].children[rowB-1].children[0].value)){
+    works = 0;
+  }
+}
+else if(((rowA == 2) || (rowA == 5) || (rowA == 8)) && ((rowB == 1) || (rowB == 4) || (rowB == 7))){
+  if(randNumber == (a[rowA-1][0].children[rowB+1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA-1][0].children[rowB-1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA-2][0].children[rowB+1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA-2][0].children[rowB-1].children[0].value)){
+    works = 0;
+  }
+}
+else if(((rowA == 2) || (rowA == 5) || (rowA == 8)) && ((rowB == 2) || (rowB == 5) || (rowB == 8))){
+  if(randNumber == (a[rowA-1][0].children[rowB-2].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA-1][0].children[rowB-1].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA-2][0].children[rowB-2].children[0].value)){
+    works = 0;
+  }
+  else if(randNumber == (a[rowA-2][0].children[rowB-1].children[0].value)){
+    works = 0;
+  }
+}
+
+if((randCell.value == "") &&(works ==1)){ // if empty and not existing in the row
+  randCell.value = randNumber; // Fill with random value
+  i++;
   }
 }
 
